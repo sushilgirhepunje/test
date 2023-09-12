9995b055de488c0c8c67d1a841cacf91b4518296
Sushil Girhepunje <girhepunjesushil@gmail.com>
2023-09-12 18:20:23 +0530

    normal

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Banner = () => {
   return (
     <div className=" w-full h-screen flex items-center justify-center banner">
-       <h1 className='f-bold'>Exploring the Cosmos with SpaceX Capsules: Your Launchpad to the Stars!</h1> 
+       <h1 className='font-bold'>Exploring the Cosmos with SpaceX Capsules: Your Launchpad to the Stars!</h1> 
     </div>
   )
 }
